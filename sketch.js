62c5f55ab14096d8d0ccfa1e40be5853549303c1
NoNelso <jasonr4nd@gmail.com>
2019-05-29 21:58:30 -0400

    setup grid

--- sketch.js
+++ sketch.js
@@ -1,5 +1,23 @@
+let grid;
+let next;
+
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(400, 400);
+  grid = [];
+  for (var x = 0; x < width; x++) {
+    grid[x] = [];
+    next[x] = [];
+    for (var y = 0; y < height; y++) {
+      grid[y] = {
+        a: 0,
+        b: 0
+      };
+      next[y] = {
+        a: 0,
+        b: 0
+      };
+    }
+  }
 }
 
 function draw() {
