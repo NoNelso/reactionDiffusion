027682cdf3a11336afa3ca4477d31474378377b0
NoNelso <jasonr4nd@gmail.com>
2019-06-01 22:29:11 -0400

    added framework for laplace formula to alter the 'next' array

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
 var grid;
 var next;
 
+var da = 1;
+var db = 0.5;
+var feed = 0.055;
+var k = 0.062;
+
 function setup() {
   createCanvas(400, 300);
   grid = [];
@@ -27,9 +32,12 @@ function draw() {
   background(0);
   for (var x = 0; x < width; x++) {
     for (var y = 0; y < height; y++) {
-      next[x][y].a = grid[x][y].a
-      next[x][y].b = grid[x][y].b
-      next[x][y].c = grid[x][y].c
+      var a = grid[x][y].a;
+      var b = grid[x][y].b;
+      var c = grid[x][y].c;
+      next[x][y].a = a + da * lapla(x, y) - a * b * b + feed * (1 - a);
+      next[x][y].b = a + db * laplb(x, y) - a * b * b - (k + feed) * b;
+      next[x][y].c = c + random(-1, 1);
     }
   }
   loadPixels();
@@ -43,10 +51,19 @@ function draw() {
     }
   }
   updatePixels();
+  swap();
 }
 
 function swap() {
   var holder = grid;
   grid = next;
   next = holder;
+}
+
+function lapla(x, y) {
+  //do the thing
+}
+
+function laplb(x, y) {
+  //do the other thing
 }
